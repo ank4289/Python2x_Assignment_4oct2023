1ad39214fc0faf1a59cf3a5cda0782aa83f68a0b
ank4289 <87062687+ank4289@users.noreply.github.com>
2023-10-06 07:14:46 +0530

    Add files via upload
    added code

---
+++ Assignment task 2.py
@@ -0,0 +1,9 @@
+name=input("Enter Your Name")
+print(name)
+
+Firstname= "Ankit"
+LastName = "Sharma"
+print(Firstname,LastName, sep='-',end='\t''\n' )
+
+NewName= input("Please enter your name")
+print("You are welcome "+NewName+" to the Python Class.")
